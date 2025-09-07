77fe863039891fab3f390e534ee54d63913c8174
Olivier-debug <ruan.olivier2@gmail.com>
2025-09-07 21:59:33 +0200

    Delete lib directory

--- lib/utils/write_temp_file_web.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:typed_data';
-
-Future<String> saveBytesToTempFile(Uint8List bytes, String fileName) async {
-  // Not used on web (we upload bytes directly). Return a harmless token.
-  return fileName;
-}
