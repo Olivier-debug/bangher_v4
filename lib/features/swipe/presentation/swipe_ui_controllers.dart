1275709698560b690072c00cdf3a30ba70736423
Olivier-debug <ruan.olivier2@gmail.com>
2025-09-28 11:17:14 +0200

    Delete lib directory

--- lib/features/swipe/presentation/swipe_ui_controllers.dart
+++
@@ -1,9 +0,0 @@
-// lib/features/swipe/presentation/swipe_ui_controllers.dart
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:swipable_stack/swipable_stack.dart';
-
-final swipeStackControllerProvider = Provider<SwipableStackController>((ref) {
-  final ctrl = SwipableStackController();
-  ref.onDispose(ctrl.dispose);
-  return ctrl;
-});
